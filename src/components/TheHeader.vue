3672c0cdc7a892402ba3f8c6189b19f4b8e1ca89
Ruan Silva <ruan.silva@ditech.com.br>
2023-03-09 22:43:14 -0300

    criando componente de header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Header</h1>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'TheHeader',
+    }
+
+
+</script>
+
+<style scoped>
+body{
+background-color: black;
+}
+</style>
