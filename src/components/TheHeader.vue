2215f7732cdc040fcc631d24ccb8cbeb5eb5abe6
Ruan Silva <ruansilva6721@gmail.com>
2023-03-11 23:38:42 -0300

    aula 11

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,43 @@
 <template>
     <div>
         <h1>Header</h1>
+        <input type="text" v-model="name" > 
+        {{ name }}
+
+        <br><br><br>
+        <select v-model="pageCount">
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        </select>
+        {{ pageCount }}
     </div>
 </template>
 <script>
     export default {
         name: 'TheHeader',
+        data(){
+            return {
+                name: 'Ruan',
+                pageCount: 5
+            }
+        },
+        watch:{
+            name(newValue, oldValue){
+
+                this.savaName(newValue, oldValue)
+            },
+            pageCount(){
+                console.log('Ajax')
+            }
+        },
+        methods:{
+            savaName(newValue, oldValue){
+                console.log(newValue, oldValue)
+                console.log('teste')
+            }
+        }
+
     }
 
 
